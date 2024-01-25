2eaabe223d1fb73bd719d31a3f6797cbe8a69009
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-01-25 15:41:10 +0530

    arrays, tuples, enums

--- types.ts
+++ types.ts
@@ -10,3 +10,25 @@ function sum(num1: number, num2: number): number {
 }
 
 sum("One", 2);
+
+let unknown: any = 20;
+unknown = "Twenty";
+
+let array: any[] = [1, true, "Jon"];
+
+let numbers: Array<number> = [1, 2, 3, 4, "5"];
+
+let person: [string, boolean] = ["John", 35];
+
+let taskStatus: TaskStatus;
+
+enum TaskStatus {
+  ToBeScheduled,
+  New,
+  Completed,
+  InProgress,
+}
+
+taskStatus = TaskStatus.Completed;
+
+// New, ToBeScheduled, Completed, InProgress.
