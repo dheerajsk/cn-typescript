07e935585cb90e7a2eb36f2376b6a428d4188333
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-01-25 15:15:13 +0530

    basic types

---
+++ types.ts
@@ -0,0 +1,12 @@
+let isActive = true;
+
+let total = 100.9;
+total = "Hundred";
+
+let fullname: string = "John Doe";
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+sum("One", 2);
